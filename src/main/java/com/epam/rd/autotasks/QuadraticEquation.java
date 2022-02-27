e5012a2228a796bf3653de51b2f6af8f71a7970a
Danil Dyachkov <danildyachkov13@gmail.com>
2022-02-27 15:41:44 +0000

    Update src/main/java/com/epam/rd/autotasks/QuadraticEquation.java

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -6,13 +6,23 @@ import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
     public static void main(String[] args) {
+        final int DISCRIMINANT_AC_MULTIPLIER = 4;
+        final int QUOTIENT_MULTIPLIER = 2;
+        final double DOUBLE_MARGIN = 1E-7;
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        double d = b * b - DISCRIMINANT_AC_MULTIPLIER * a * c;
+        System.out.println(d);
+        if (Math.abs(d) < DOUBLE_MARGIN)  {
+            System.out.println(-b / (QUOTIENT_MULTIPLIER * a));
+        } else if (d > 0) {
+            System.out.println(-b + sqrt(d) / (QUOTIENT_MULTIPLIER * a));
+            System.out.print(-b - sqrt(d) / (QUOTIENT_MULTIPLIER * a));
+        } else {
+            System.out.println("no roots");
+        }
     }
 
 }
